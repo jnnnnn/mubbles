939748e35ebb33461587adda86dac81e46778a28
jnnnnn <jnnnnn@gmail.com>
2023-05-07 15:12:41 +1200

    try soundio

--- devlog.md
+++ devlog.md
@@ -5,3 +5,13 @@
 started dev. I tried out a few other whisper desktop things but they don't work very well. 
 
 I also didn't find one that will record what both the mic and the speakers are saying, in two separate columns. I feel like that's a pretty cool feature.
+
+## 2023-05-07
+
+First step: get the audio from the mic and the speakers. Seems like the best rust audio library is soundio.  See if I can get it to work at all.
+
+Soundio doesn't compile:
+
+    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\include\stdatomic.h(15,1): fatal  error C1189: #error:  <stdatomic.h> is not yet supported when compiling as C, but this is planned for a future release. [C:\Users\J\Source\subbles\target\debug\build\libsoundio-sys-e7b60699a3621a70\out\build\libsoundio_static.vcxproj]
+
+Google (and copilot) suggests cpal. Try that instead.
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,62 @@ fn main() -> eframe::Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
+    let result = dosoundiotest();
+
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "subbles",
         native_options,
         Box::new(|cc| Box::new(subbles::TemplateApp::new(cc))),
     )
+}
+
+fn dosoundiotest() -> soundio::Result {
+    let ctx = soundio::Context::new();
+    ctx.set_app_name("Subbles or Mubbles");
+    ctx.connect()?;
+    ctx.flush_events();
+
+    let input_devices = ctx.input_devices()?;
+    for dev in input_devices.iter() {
+        println!("Input device: {}", dev.name());
+    }
+
+    let dev = ctx.default_input_device().expect("No input device");
+    let mut input_stream = dev.open_instream(
+        44100,
+        soundio::Format::S16LE,
+        soundio::ChannelLayout::get_builtin(soundio::ChannelLayoutId::Stereo),
+        1.0,
+        read_callback,
+        None::<fn()>,
+        None::<fn(soundio::Error)>,
+    )?;
+
+    input_stream.start()?;
+}
+
+fn read_callback(stream: &mut soundio::InStreamReader) {
+    let frame_count_max = stream.frame_count_max();
+    if let Err(e) = stream.begin_read(frame_count_max) {
+        println!("Error reading from stream: {}", e);
+        return;
+    }
+
+    let mut maxsample = 0;
+    for f in 0..stream.frame_count() {
+        for c in 0..stream.channel_count() {
+            let sample = stream.sample::<i16>(c, f);
+            if sample > maxsample {
+                maxsample = sample;
+            }
+        }
+    }
+    println(
+        "Callback, frame count: {}, max: {}",
+        stream.frame_count(),
+        maxsample,
+    );
 }
 
 // when compiling to web using trunk.
